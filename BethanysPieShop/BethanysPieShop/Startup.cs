0239038071c49deee6eb3d925e4e87ed005682fc
Dan Dumitriu <dandumitriu33@gmail.com>
2020-09-19 14:13:33 +0300

    Configure DbContext service in Startup

--- BethanysPieShop/BethanysPieShop/Startup.cs
+++ BethanysPieShop/BethanysPieShop/Startup.cs
@@ -6,6 +6,8 @@ using BethanysPieShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -13,6 +15,11 @@ namespace BethanysPieShop
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -23,6 +30,8 @@ namespace BethanysPieShop
             // services.AddTransient - new instance every time
             // services.AddScoped - singeton per request
             // services.AddSingleton - unique instance
+            services.AddDbContext<AppDbContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
